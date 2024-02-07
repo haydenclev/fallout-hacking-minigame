8fa9d160e4b9c365c97acc9795a4cd4e726fc87a
Hayden <hayden.clev@gmail.com>
2024-02-07 12:45:17 -0800

    refactors for clarity and convention

--- src/hack.java
+++ src/hack.java
@@ -14,15 +14,34 @@ import static src.utils.*;
 public class hack {
 
 	//fields
-	private static int LINE = 20;	//num chars in one column
-	private static int ROW = 40;	//num of rows total (or num rows in one column * 2)
+	private static int CHARACTERS_PER_COLUMN = 20;
+	private static int TOTAL_ROWS = 40;	//or num rows in one column * 2
 	private static int NUM_WORDS = 10;
 	private static int GUESS_LIMIT = 4;
 	private static int WORD_LEN = 4;
 
 	 public static void main(String[] args) {
+		 checkCommandLineArguments(args);
 
-		//checking command line args
+		 Scanner scan = new Scanner(System.in);
+		 String choice = "y";
+
+		 while(choice.equals("y")) {
+			boolean playerWins = runGame();
+			if(playerWins)
+				gameWin();
+			else
+				gameLose();
+			System.out.println("continue? (y/n)");
+			choice = scan.next();
+		}
+	}
+
+	/**
+	 * Method to make sure command line arguments are properly formatted and set game variables based on them
+	 * @param args is the command line arguments
+	 */
+	private static void checkCommandLineArguments(String[] args) {
 		if(	args.length != 1
 			|| ( !args[0].equals("easy")
 			&& !args[0].equals("medium")
@@ -34,8 +53,8 @@ public class hack {
 		}
 		else if(args[0].equals("extreme")) {
 			WORD_LEN = 7;
-			LINE = 60;
-			ROW = 80;
+			CHARACTERS_PER_COLUMN = 60;
+			TOTAL_ROWS = 80;
 			NUM_WORDS = 50;
 		}
 		else if(args[0].equals("hard"))
@@ -44,36 +63,14 @@ public class hack {
 			WORD_LEN = 5;
 		else
 			WORD_LEN = 4;
-
-		Scanner scan = new Scanner(System.in);
-		String choice = "y";
-
-		//allowing user to continue
-		while(choice.equals("y")) {
-
-			//running actual game
-			int win = runGame();
-
-			//checking for win or loss
-			if(win == 1)
-				gameWin();
-
-			else
-				gameLose();
-
-			//continue?
-			System.out.println("continue? (y/n)");
-			choice = scan.next();
-		}
 	}
 
 
-
 	/**
-	Method to run the actual game
+	 * Method to run the actual game
+	 * @return true if player wins, false otherwise
 	*/
-	private static int runGame() {
-		//variables to handle game board and state
+	private static boolean runGame() {
 		char[] grid = makeGrid();
 		Set<String> words = makeWords();
 		grid = inputWords(grid, words);
@@ -85,24 +82,17 @@ public class hack {
 
 		while(guessCount < GUESS_LIMIT) {
 			print(grid, addresses, guesses, guessCount);
-
-			//reading in user input
 			String guess = scan.next();
-
 			if(words.contains(guess)) {
-				//checking for user match
-				int likeness = likeness(guess, password);
+				int numberOfMatchingCharacters = similarity(guess, password);
+				if (guessMatchesPassword(numberOfMatchingCharacters)) {
+					return true;
+				}
 
-				//checking if game is over
-				if (likeness == WORD_LEN) {
-					return 1;
-				}    //win
+				//TODO: cheat codes
 
-				//handle braces
-				//handle likeness
-
-				//add user input to guesses
-				guesses.add("INPUT: " + guess + " LIKENESS: " + likeness);
+				//add user input to guesses for display
+				guesses.add("INPUT: " + guess + " LIKENESS: " + numberOfMatchingCharacters);
 
 				guessCount++;
 			}
@@ -113,7 +103,16 @@ public class hack {
 
 		}
 		System.out.println("password: " + password);
-		return -1;	//lose
+		return false;
+	}
+
+	/**
+	 * Method to see if player guessed password
+	 * @param numberOfMatchingCharacters is the number of characters in the right position and the right value between the guess and the password
+	 * @return true if guess matches the password otherwise false
+	 */
+	private static boolean guessMatchesPassword(int numberOfMatchingCharacters) {
+		return numberOfMatchingCharacters == WORD_LEN;
 	}
 
 	/**
@@ -137,12 +136,12 @@ public class hack {
 	}
 
 	/**
-	Method to check likeness of user input
-	@param choice is the user input
-	@param password is the password
-	@return the likeness of the input and password
+	 * Method to check likeness of user input
+	 * @param choice is the user input
+	 * @param password is the password
+	 * @return the likeness of the input and password
 	*/
-	private static int likeness(String choice, String password) {
+	private static int similarity(String choice, String password) {
 		int likeness = 0;
 		int length = choice.length() < WORD_LEN ? choice.length() : WORD_LEN;
 		for(int i = 0; i < length; i++) {
@@ -154,14 +153,14 @@ public class hack {
 	}
 
 	/**
-	Method to put the words in the grid
-	@param grid is the game grid of ascii chars
-	@param words is the Set<String> of words
+	 * Method to put the words in the grid
+	 * @param grid is the game grid of ascii chars
+	 * @param words is the Set<String> of words
 	*/
 	private static char[] inputWords(char[] grid, Set<String> words) {
 
 		Random r = new Random();
-		int spacing = (LINE * ROW) / NUM_WORDS;	//frequency of words
+		int spacing = (CHARACTERS_PER_COLUMN * TOTAL_ROWS) / NUM_WORDS;	//frequency of words
 		//check for frequency vs word length conflict
 		if(spacing < WORD_LEN) {
 			System.out.println("Spacing and Word Length conflict");
@@ -183,8 +182,8 @@ public class hack {
 	}
 
 	/**
-	Method to create the array of words that appear in the grid
-	@return words a Set<String> of words
+	 * Method to create the array of words that appear in the grid
+	 * @return words a Set<String> of words
 	*/
 	private static Set<String> makeWords() {
 		Random r = new Random();
@@ -209,8 +208,8 @@ public class hack {
 	}
 
 	/**
-	Method to count the number of lines contained in a dictionary file
-	@return size which is the number of words in the dictionary file
+	 * Method to count the number of lines contained in a dictionary file
+	 * @return size which is the number of words in the dictionary file
 	*/
 	private static int getDictSize() {
 		int count = 0;
@@ -225,17 +224,17 @@ public class hack {
 	}
 
 	/**
-	Method to greate the grid of random ascii values
-	@return grid the game array of chars
+	 * Method to greate the grid of random ascii values
+	 * @return grid the game array of chars
 	*/
 	private static char[] makeGrid() {
 
-		char[] grid = new char[ROW * LINE];
+		char[] grid = new char[TOTAL_ROWS * CHARACTERS_PER_COLUMN];
 
 		Random r = new Random();
 
 		//populating array
-		for(int i = 0; i < LINE*ROW; i++) {
+		for(int i = 0; i < CHARACTERS_PER_COLUMN * TOTAL_ROWS; i++) {
 			int check = 65;
 			while(((check < 58) && (check > 47))	//removing ascii codes 48-57
 			|| ((check < 91) && (check > 63)) 		//removing ascii codes 64-90
@@ -249,13 +248,13 @@ public class hack {
 	}
 
 	/**
-	Method to generate the memory addresses
-	They appear between the two columns of data
+	 * Method to generate the memory addresses
+	 * They appear between the two columns of data
 	*/
 	private static int[] getAddresses() {
 		Random r = new Random();
 		int start = r.nextInt(0x7B48) + 0x7B48;
-		int[] addresses = new int[ROW];
+		int[] addresses = new int[TOTAL_ROWS];
 		addresses[0] = start;
 		for(int i = 1; i < addresses.length; i++) {
 			addresses[i] = addresses[i-1] + 0x06;
@@ -264,34 +263,34 @@ public class hack {
 	}
 
 	/**
-	Method to print the grid to the terminal window
-	@param grid is the char array to be printed
-	@param addresses is the array of computer memory addresses
-	@param guesses is the List<String> of user guesses
-	@param chances is the number of tries so far by the user
+	 * Method to print the grid to the terminal window
+	 * @param grid is the char array to be printed
+	 * @param addresses is the array of computer memory addresses
+	 * @param guesses is the List<String> of user guesses
+	 * @param chances is the number of tries so far by the user
 	*/
 	private static void print(char[] grid, int[] addresses, List<String> guesses, int chances) {
 
 		clear();
 
-		for(int i = 0; i < ROW/2; i++) {
+		for(int i = 0; i < TOTAL_ROWS /2; i++) {
 
 			//printing address column
 			System.out.printf("0x" + "%x" + "\t", addresses[i]);
 
 			//printing first data column
-			for(int j = 0; j < LINE; j++) {
-				System.out.print(grid[(i * LINE) + j]);
+			for(int j = 0; j < CHARACTERS_PER_COLUMN; j++) {
+				System.out.print(grid[(i * CHARACTERS_PER_COLUMN) + j]);
 			}
 
 			System.out.print("\t");
 
 			//printing second address column
-			System.out.printf("0x" + "%x" + "\t", addresses[ROW/2+i]);
+			System.out.printf("0x" + "%x" + "\t", addresses[TOTAL_ROWS /2+i]);
 
 			//printing second data column
-			for(int j = 0; j < LINE; j++) {
-				System.out.print(grid[(i * LINE) + j + grid.length/2]);
+			for(int j = 0; j < CHARACTERS_PER_COLUMN; j++) {
+				System.out.print(grid[(i * CHARACTERS_PER_COLUMN) + j + grid.length/2]);
 			}
 
 			System.out.println();
