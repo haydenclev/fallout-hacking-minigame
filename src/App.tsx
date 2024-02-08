bc935ddbe38c7ea0e3a286b2fc11c31d234efdf4
Hayden <hayden.clev@gmail.com>
2024-02-08 12:19:05 -0800

    re-arrange file structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        <code>RobCo Industries Terminal Hacking Minigame</code>
+      </p>
+    </div>
+  );
+}
+
+export default App;
