7ced75ec287afcd28f25aefaf3896741ae3ad6e6
Hayden <hayden.clev@gmail.com>
2024-03-07 20:07:39 -0800

    style: attempts bar styling and warning message blinks #6

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,19 +5,19 @@ interface HeaderProps {
   guessLimit: number,
 }
 
-function Header({guessCount, guessLimit}: HeaderProps) {
+function Header({ guessCount, guessLimit }: HeaderProps) {
   const headerMessage = "robco industries (tm) termlink protocol";
   const warningMessage = "!!! warning: lockout imminent !!!";
   const regularMesage = "password required";
-  const squareCharacter = "\u25A1"; // convert to 250A0
+  const squareCharacter = "\u25A0";
   let attemptsArray = new Array(guessLimit - guessCount).fill(squareCharacter);
 
   const displaywarningMessage = guessLimit - guessCount === 1 ? true : false;
   return (
     <div>
       <p>{headerMessage}</p>
-      <p>{displaywarningMessage ? warningMessage : regularMesage}</p>
-      <p id="attempts">attempts remaining: {attemptsArray}</p>
+      {displaywarningMessage ? <p className="blink">{warningMessage}</p> : <p>{regularMesage}</p>}
+      <p id="attempts">attempt(s) remaining: {attemptsArray.join(" ")}</p>
     </div>
   )
 }
