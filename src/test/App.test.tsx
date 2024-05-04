fb0cf9cda30de5bb333d46e436f38aacb0787168
Hayden <hayden.clev@gmail.com>
2024-05-04 11:01:48 -0700

    test: add tests for global settings #12

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import { App } from '../components/App';
+import { GlobalContext } from '../components/Context';
 
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  expect(() => render(<App />)).not.toThrow();
 });
 
-// export {} // makes file module to remove compile error
+describe('global context', () => {
+  beforeEach(() => {
+    console.error = jest.fn();
+  })
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  })
+
+  const globalSettings = {
+    CHARACTERS_PER_ROW: 12,
+    GUESS_LIMIT: 4,
+    NUM_WORDS: 12,
+    TOTAL_ROWS: 17,
+    WORD_LEN: 7,
+  }
+
+  it('fails when words cant fit in grid', () => {
+    expect(() => {
+      render(
+        <GlobalContext.Provider value={{...globalSettings, NUM_WORDS: 1000}} >
+          <App />
+        </GlobalContext.Provider>
+      );
+    }).toThrow('Not enough room to place words in grid');
+  })
+
+  it('fails when grid length not even number', () => {
+    expect(() => {
+      render(
+        <GlobalContext.Provider value={{...globalSettings, CHARACTERS_PER_ROW: 13}} >
+          <App />
+        </GlobalContext.Provider>
+      );
+    }).toThrow('Grid length must be divisible into two even columns');
+  })
+
+  it('fails when word could be split across the two columns', () => {
+    expect(() => {
+      render(
+        <GlobalContext.Provider value={{...globalSettings, NUM_WORDS: 13}} >
+          <App />
+        </GlobalContext.Provider>
+      );
+    }).toThrow('Word frequency must evenly divide grid length');
+  })
+})
